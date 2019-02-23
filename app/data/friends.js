469c1f57f93ec05dc739cc760e80f3658c7c8c47
Brandon Dixon <bdixon850@yahoo.com>
2019-02-22 23:13:15 -0600

    update

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Carolyn',
+		photo: 'http://media.columbiarecords.com/solange/magazinecovers/solange-nyt.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Brandon',
+		photo: 'https://www.billboard.com/files/media/drake-smile-nba-awards-2017-a-billboard-1548.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jordan',
+		photo: 'https://www.star2.com/wp-content/uploads/2018/11/Justin-770x470.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Dylan',
+		photo: 'https://ichef.bbci.co.uk/news/660/cpsprodpb/E225/production/_93339875_obamalaughing.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jasmine',
+		photo: 'https://cdn.cliqueinc.com/cache/posts/273686/tia-mowry-parenting-tips-273686-1543423995261-main.700x0c.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'LaToya',
+		photo: 'https://7lwy5tgst9-flywheel.netdna-ssl.com/wp-content/uploads/2016/09/LetoyaLuckett_Header.jpeg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Trina',
+		photo: 'https://res-5.cloudinary.com/revolttv/image/upload/w_800/c_crop,f_auto,fl_lossy.force_strip,q_auto:best,h_450,w_800,x_0,y_0/rgmsbzexdip3pa9ugunh.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'John Legend',
+		photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwMjg5NDYwNzUw/john-legend-201302-1-402.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lauryn Hill',
+		photo: 'https://www.billboard.com/files/styles/article_main_image/public/media/lauryn-hill-1998-billboard-1548.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Tyra Banks',
+		photo: 'https://9p06z2ofdzx3wbhgp1ccaea1-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/NUP_102148_0969.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
